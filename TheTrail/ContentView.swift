b32fc058fae8ff2c65bc731d30ef72c3ca49712e
morrowwc <65681991+morrowwc@users.noreply.github.com>
2021-09-14 12:56:26 -0500

    Initial Commit

---
+++ TheTrail/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheTrail
+//
+//  Created by Will Morrow on 9/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheTrail/TheTrailApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheTrailApp.swift
+//  TheTrail
+//
+//  Created by Will Morrow on 9/14/21.
+//
+
+import SwiftUI
+
+@main
+struct TheTrailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
